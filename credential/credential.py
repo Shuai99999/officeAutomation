d6cf240dbbb5a1ac82fb27e3c1168ed834fd0a92
Shuai99999 <gaoshuai099@gmail.com>
2024-07-08 17:31:06 +0800

    1

--- credential/credential.py
+++ credential/credential.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 credential = 'eyJzdWJqZWN0IjoiMDE0NTQyMjEiLCJwYXNzd29yZCI6ImQybHVaRzkzYzBBeE13PT0iLCJ0eXBlIjoxfQ%3D%3D'
 credential_gaoshuai = credential
-# credential_diaowentao = 'eyJzdWJqZWN0IjoiMjMwMzI5MTkiLCJwYXNzd29yZCI6IlJGZFVNVGs1TmpBeE1qbGtkM1E9IiwidHlwZSI6MX0%3D'
-credential_diaowentao = 'ey'
+credential_diaowentao = 'eyJzdWJqZWN0IjoiMjMwMzI5MTkiLCJwYXNzd29yZCI6IlJGZFVNVGs1TmpBeE1qbGtkM1E9IiwidHlwZSI6MX0%3D'
+# credential_diaowentao = 'ey'
