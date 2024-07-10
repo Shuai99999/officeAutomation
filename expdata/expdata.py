0c3bc2b51b14bc8adf4bdad0533d5eacffbc96a4
Shuai99999 <gaoshuai099@gmail.com>
2024-07-10 17:13:23 +0800

    1

---
+++ expdata/expdata.py
@@ -0,0 +1,107 @@
+import requests
+import json
+import sys
+import subprocess
+sys.path.insert(0, sys.path[0]+"/../credential")
+from credential import credential
+
+# credential = "eyJzdWJqZWN0IjoiMDE0NTQyMjEiLCJwYXNzd29yZCI6ImQybHVaRzkzYzBBeE1nPT0iLCJ0eXBlIjoxfQ%3D%3D"
+iam_token_url = "https://iama.haier.net/api/oauth/authorize?client_id=cb3b02710dd7ebd0b10762405121d418&credential=" + credential + "&loginType=2&redirect_uri=https%3A%2F%2Frrsoa.rrswl.com%2Fuuc.html%3F%24%24query%24%24eyJyZWRpcmVjdCI6ImluZGV4In0%40%40%24%24end%24%24"
+
+iam_token_payload = {}
+iam_token_headers = {
+    'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+    'languageEnv': 'cn',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+    'Content-Type': 'application/json;charset=utf-8',
+    'Accept': 'application/json, text/plain, */*',
+    'Application-Key': 'cb3b02710dd7ebd0b10762405121d418',
+    'sec-ch-ua-platform': '"Windows"',
+    'host': 'iama.haier.net'
+}
+
+iam_token_get = requests.get(iam_token_url, headers=iam_token_headers, data=iam_token_payload)
+
+iam_token = json.loads(iam_token_get.text)
+
+iam_token = iam_token.get('data').get('access_token')
+
+token_url = "https://rrsoa.rrswl.com/uniedp-web/open/api/authLogin/authToken"
+
+token_payload = json.dumps({
+    "accessToken": iam_token
+})
+
+token_headers = {
+    'sec-ch-ua': '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
+    'Accept': 'application/json, text/plain, */*',
+    'Content-Type': 'application/json',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
+    'sec-ch-ua-platform': '"Windows"'
+}
+
+task_token = requests.request("POST", token_url, headers=token_headers, data=token_payload)
+
+task_token = json.loads(task_token.text)
+
+task_token = task_token.get('data').get('token')
+
+headers = {
+    'Host': 'rrsoa.rrswl.com',
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Connection': 'keep-alive',
+    'Content-Type': "application/json",
+    'Referer': 'https://rrsoa.rrswl.com',
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+    'Token': task_token,
+}
+
+url = 'https://rrsoa.rrswl.com/uniedp-web/oa/flowable/processInst/page?order=&orderField=&sumFields=&page=1&limit=1000&source=todo-1&formCode=&formName=&procTitle=&procName=&beginDateStr=&endDateStr=&status=&startBy='
+
+todo_list_url = requests.get(url=url, headers=headers)
+
+todo_list = json.loads(todo_list_url.text)
+
+for i in todo_list.get('data').get('list'):
+    taskId = i.get('taskId')
+    instId = i.get('id')
+    procBizCode = i.get('procBizCode')
+    procDefName = i.get('procDefName')
+    if procDefName == '数据获取':
+        task_url = 'https://rrsoa.rrswl.com/uniedp-web/oa/flowable/taskInst/getFormData?taskId=' + taskId
+        r_task_detail = requests.get(url=task_url, headers=headers)
+        task_details = json.loads(r_task_detail.text)
+        task_detail = json.loads(task_details.get('data').get('boData'))
+        fileId = task_detail.get('sqlFileId')
+        sqlRemarks = task_detail.get('sqlRemarks')
+        db_name = task_detail["databaseUrl"]
+
+        if '10.246.2.160' in db_name:
+            db = 'crm_exp'
+            db_type = 'mysql'
+        else:
+            db = ''
+            db_type = ''
+
+        if fileId:
+            fileIdArray = fileId.split(',')
+            for fileIdArrayItem in fileIdArray:
+                attach_url = 'https://rrsoa.rrswl.com/uniedp-web/obs/download?ossId=' + fileIdArrayItem + '&token=' + task_token + '&charset=UTF-8'
+                attach = requests.get(attach_url)
+                with open('/home/' + db_type + '/dba/bi/inputs', "wb") as code:
+                    # with open('input.txt', "wb") as code:
+                    code.write(attach.content)
+                # os.system('read -n 1')
+
+        else:
+            sql_text = sqlRemarks
+            sql_text = sql_text.encode()
+            with open('/home/' + db_type + '/dba/bi/input', "wb") as code:
+                code.write(sql_text)
+
+        if db:
+            subprocess.Popen(['su', '-', db_type, '/home/' + db_type + '/dba/bi/multi_exp.sh', db, procBizCode, " > /dev/null &"], stdout=subprocess.PIPE)
+            print('tail -1 /home/' + db_type + '/dba/bi/rpt')
+
---
+++ expdata/expdata.sh
@@ -0,0 +1,1 @@
+/usr/bin/python3 /root/python/officeAutomation/expdata/expdata.py >> /root/python/officeAutomation/expdata/expdata.log
