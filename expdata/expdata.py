7f0332f0c1a9f1387d7273da8e868dea3e885d7e
gaoshuai <gaoshuai099@gmail.com>
2024-08-12 17:24:31 +0800

    1

--- expdata/expdata.py
+++ expdata/expdata.py
@@ -112,7 +112,7 @@ for i in todo_list.get('data').get('list'):
             db = 'oracle_cdk_exp'
             db_type = 'oracle'
             fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:cdkexp 密码:8UytK6Z*toY5，查找文件：'
-        elif 'oracle_vom2_exp' in db_name or 'oracle_vom2_exp' in db_url:
+        elif 'oracle_vom2_exp' in db_name or 'wloms2' in db_url:
             db = 'oracle_vom2_exp'
             db_type = 'oracle'
             fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:omsexp 密码:7wS&$M7ffGfLdg93，查找文件：'
@@ -143,6 +143,10 @@ for i in todo_list.get('data').get('list'):
         elif 'oracle_wldtm_exp' in db_name:
             db = 'oracle_wldtm_exp'
             db_type = 'oracle'
+            fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:iwmsexp 密码:h$6m$LzBZESvwTcr，查找文件：'
+        elif 'iwmsdb_exp' in db_name:
+            db = 'iwmsdb_exp'
+            db_type = 'mysql'
             fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:iwmsexp 密码:h$6m$LzBZESvwTcr，查找文件：'
         elif 'ldg' in db_name:
             db = 'mycat_bms8066_ldgdb_exp'
