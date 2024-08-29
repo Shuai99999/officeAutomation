2c54f3f4709277e0e887f827e823c5289cd6a593
gaoshuai <gaoshuai099@gmail.com>
2024-08-29 13:39:19 +0800

    1

--- expdata/expdata.py
+++ expdata/expdata.py
@@ -96,6 +96,10 @@ for i in todo_list.get('data').get('list'):
             db = 'mycat_oms_otm_exp'
             db_type = 'mysql'
             fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:omsexp 密码:7wS&$M7ffGfLdg93，查找文件：'
+        elif 'mysql_oms_cus_st_exp' in db_name:
+            db = 'mysql_oms_cus_st_exp'
+            db_type = 'mysql'
+            fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:omsexp 密码:7wS&$M7ffGfLdg93，查找文件：'
         elif 'oracle_arch_oms_exp' in db_name:
             db = 'oracle_arch_oms_exp'
             db_type = 'oracle'
