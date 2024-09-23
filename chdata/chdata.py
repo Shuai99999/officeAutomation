bd97da4ecd43aa4cf1002088fa37e5fdd7f23286
gaoshuai <gaoshuai099@gmail.com>
2024-09-23 16:53:37 +0800

    1

--- chdata/chdata.py
+++ chdata/chdata.py
@@ -120,7 +120,7 @@ for i in todo_list.get('data').get('list'):
         elif 'i3wms' in db_name:
             db = 'i3wms_pr'
             db_type = 'oracle'
-        elif 'i4wms' in db_name:
+        elif 'i4wms' in db_name or 'iwms04' in db_name:
             db = 'iwms04_pr'
             db_type = 'oracle'
         elif 'rrslesdb' in db_name:
