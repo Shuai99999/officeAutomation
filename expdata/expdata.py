417cc20e013ab5fce2a6a6dc9763a62f14f10380
gaoshuai <gaoshuai099@gmail.com>
2024-11-14 08:34:56 +0800

    1

--- expdata/expdata.py
+++ expdata/expdata.py
@@ -80,7 +80,7 @@ for i in todo_list.get('data').get('list'):
         db_url = task_detail["databaseUrl"]
         db_name = task_detail["databaseName"]
 
-        if '10.246.2.160' in db_name:
+        if '10.246.2.160' in db_name or '10.246.2.160' in db_url:
             db = 'crm_exp'
             db_type = 'mysql'
             fileInfo = '您的数据已导出，n请登录rrswl导数ftp，打开文件资源管理器（任意文件夹），输入地址 ftp://10.135.30.96/ 输入 用户名:omsexp 密码:7wS&$M7ffGfLdg93，查找文件：'
